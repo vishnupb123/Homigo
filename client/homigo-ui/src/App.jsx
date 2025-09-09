e6039798ea024c41b1f5aeaac4e71b961dafe28d
Vishnu PB <vishnupb@Mac.home>
2025-09-09 18:56:49 +0100

    navbar

--- client/homigo-ui/src/App.jsx
+++ client/homigo-ui/src/App.jsx
@@ -1,9 +1,12 @@
+import Navbar from './components/navbar/Navbar'
+import './index.scss'
+import './layout.scss'
 import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      Hello World !
+    <div className="layout">
+      <Navbar />
     </div>
   )
 }
